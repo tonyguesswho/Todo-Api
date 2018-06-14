be30084c698ec48406a4f191ad3944b3d8f1b437
Austin <austin5astro@gmail.com>
2018-06-14 19:00:06 +0100

    initial commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,18 @@
+const MongoClient=require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db)=>{
+    if(err){
+       return console.log('unable to conect');
+    }
+    console.log('connected to mongodb');
+    db.collection('Todos').insertOne({
+        text:'lay my bed',
+        completed:false
+    },(err,result)=>{
+        if(err){
+         return  console.log('unable to add document')
+        }
+        console.log(result.ops);
+    })
+    db.close();
+});
